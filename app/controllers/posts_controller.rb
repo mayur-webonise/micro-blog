04dbae55814499825dd83edce0480fc681b15ff4
MayurM-weboniselab <mayur.mahajan@weboniselab.com>
2023-03-06 17:59:05 +0530

    feature-rails-crud | made changes in friendly_id and used dry principle in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
   def index
     @post=Post.all
   end
 
-  def show
-    @post = Post.friendly.find(params[:id])
-    
+  def show; 
   end
 
   def new
@@ -22,13 +21,10 @@ class PostsController < ApplicationController
     end
   end
 
-  def edit
-    @post = Post.find(params[:id])
+  def edit;
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to @post
     else
@@ -37,22 +33,13 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
 
     redirect_to root_path, status: :see_other
   end
 
-  private
-    def post_params
-      params.require(:post).permit(:title, :content)
-    end
-
-    def mark_notifications_as_read
-      if current_user
-        notifications_to_mark_as_read = @post-notification_as_post.where(recipient: current_user)
-        notifications_to_mark_as_read.update_all(read_at: Time.zone.now)
-      end
-    end
+  def set_post
+    @post = Post.friendly.find(params[:id])
+  end
 end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,10 @@
 class Post < ApplicationRecord
   extend FriendlyId
-friendly_id :title, use: :slugged
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 2 }
-  
+  friendly_id :title, use: %i[slugged]
 
-  def should_generate_new_friendly_id
+  def should_generate_new_friendly_id?
     title_changed? || slug.blank?
   end
 end
